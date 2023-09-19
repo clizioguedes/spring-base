ffffdc79f1699a4e146c993e63eac3120f7da111
Ramon <rsanttos22@gmail.com>
2023-09-19 13:27:08 -0300

    First commit

---
+++ src/main/java/com/acer/sellercenter/sellercenter/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.acer.sellercenter.sellercenter;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SellercenterApplication.class);
+	}
+
+}
---
+++ src/test/java/com/acer/sellercenter/sellercenter/SellercenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acer.sellercenter.sellercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellercenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
