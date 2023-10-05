75e5fd0034ccffe8e574970f61b2a1f4051ce494
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-10-05 14:45:07 -0300

    feat: remove product classes for ms base

--- src/main/java/com/acer/sellercenter/sellercenter/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.acer.sellercenter.sellercenter.repository;
-
-import com.acer.sellercenter.sellercenter.model.Product;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends GenericRepository<Product> {
-}
--- src/main/java/com/acer/sellercenter/sellercenter/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.acer.sellercenter.sellercenter.service;
-
-import com.acer.sellercenter.sellercenter.dto.ProductDTO;
-import com.acer.sellercenter.sellercenter.model.Product;
-
-public interface ProductService extends GenericService<Product, ProductDTO> {
-}
