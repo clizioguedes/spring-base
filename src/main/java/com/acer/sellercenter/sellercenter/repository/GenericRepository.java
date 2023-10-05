4dba19ca6147964146518c8fd20ead737d62d5d7
Felipe Lima <felipe.lima.092@ufrn.edu.br>
2023-10-05 14:47:13 -0300

    feat: add repository default methods for soft delete

--- src/main/java/com/acer/sellercenter/sellercenter/repository/GenericRepository.java
+++ src/main/java/com/acer/sellercenter/sellercenter/repository/GenericRepository.java
@@ -1,11 +1,36 @@
 package com.acer.sellercenter.sellercenter.repository;
 
 import com.acer.sellercenter.sellercenter.model.BaseEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
-import java.util.UUID;
+import java.util.Optional;
 
 @NoRepositoryBean
-public interface GenericRepository<T extends BaseEntity> extends JpaRepository<T, UUID> {
+public interface GenericRepository<T extends BaseEntity> extends JpaRepository<T, Long> {
+
+    @Override
+    @Transactional
+    default void deleteById(Long id) {
+        Optional<T> entity = findById(id);
+        if (entity.isPresent()) {
+            entity.get().setDeleted(true);
+            save(entity.get());
+        }
+    }
+
+    @Override
+    @Transactional
+    default void delete(T obj) {
+        obj.setDeleted(true);
+        save(obj);
+    }
+
+    @Override
+    @Transactional
+    default void deleteAll(Iterable<? extends T> arg0) {
+        arg0.forEach(entity -> deleteById(entity.getId()));
+    }
+
 }
